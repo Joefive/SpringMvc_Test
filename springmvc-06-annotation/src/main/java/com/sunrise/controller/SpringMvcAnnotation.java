d433bb4e4c695db4220fe94790a29bbd8d5142b1
Joefive <joefive@yeah.net>
2021-03-29 00:19:41 +0800

    add

--- springmvc-06-annotation/src/main/java/com/sunrise/controller/SpringMvcAnnotation.java
+++ springmvc-06-annotation/src/main/java/com/sunrise/controller/SpringMvcAnnotation.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SpringMvcAnnotation {
-    @RequestMapping
+    @RequestMapping("/first")
     public String first(Model m1) {
         m1.addAttribute("msg","SpringMvc annotation..!");
         return "first";   //会被视图解析器处理返回字符串
