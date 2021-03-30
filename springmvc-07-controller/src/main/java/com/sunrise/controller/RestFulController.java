0800f3e4e30eb1d0005589fa06e3035d54e23344
Joefive <joefive@yeah.net>
2021-03-30 11:36:49 +0800

    add

--- springmvc-07-controller/src/main/java/com/sunrise/controller/RestFulController.java
+++ springmvc-07-controller/src/main/java/com/sunrise/controller/RestFulController.java
@@ -2,14 +2,36 @@ package com.sunrise.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * RestFul风格使用
+ * 方法中使用@PathVariable注解，进行映射到对应的@RequestMapping中
+ * RequestMapping分类：
+ *
+ * @GetMapping == @RequestMapping(method = RequestMethod.GET)
+ * @PostMapping
+ * @DeleteMapping
+ * @PutMapping
+ * @PatchMapping
+ */
 
 @Controller
 public class RestFulController {
-    @RequestMapping("/add")
-    public String restFul(int a, int b, Model model) {
+    //原来链接方式：http://localhost:8080/s7/first?a=1&b=2
+    //RestFul方式：http://localhost:8080/s7/a/b
+    //@RequestMapping(value="/add/{a}/{b}",method = RequestMethod.GET)
+    @GetMapping("/add/{a}/{b}")
+    public String restFul1(@PathVariable int a, @PathVariable int b, Model model) {
         int str = a + b;
-        model.addAttribute("msg", "结果为：" + str);
+        model.addAttribute("msg", "结果为1：" + str);
+        return "first";
+    }
+
+    @PostMapping("/add/{a}/{b}")
+    public String restFul2(@PathVariable int a, @PathVariable String b, Model model) {
+        String str = a + b;
+        model.addAttribute("msg", "结果为2：" + str);
         return "first";
     }
 }
---
+++ springmvc-07-controller/web/a.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form action="/s7/add/1/5" method="post">
+    <input type="submit">
+</form>
+</body>
+</html>
